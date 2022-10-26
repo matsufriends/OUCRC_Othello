688ae56284e262dc5bcbb2dba1b5d3d0416c945b
matsu_friends <pikapikan0526@gmail.com>
2022-10-26 09:07:53 +0900

    TMP

--- Assets/_Develop/Scripts/oucrcNet/OucrcNetUtility.cs
+++ Assets/_Develop/Scripts/oucrcNet/OucrcNetUtility.cs
@@ -16,37 +16,15 @@ namespace oucrcNet {
                 uploadHandler   = new UploadHandlerRaw(postData)
                ,downloadHandler = new DownloadHandlerBuffer()
             };
-            Debug.Log(JsonMapper.ToJson(sendInfo));
-            Debug.Log(postData);
             request.SetRequestHeader("Content-Type","application/json");
             await request.SendWebRequest();
-            
             request.Dispose();
-            /*
-            var req = new UnityWebRequest($"{url}/{roomId}", "POST");
-            req.uploadHandler   = new UploadHandlerRaw(postData);
-            req.downloadHandler = new DownloadHandlerBuffer();
-            req.SetRequestHeader("Content-Type", "application/json");
-            req.Send();
-*/
-            return;
-            /*
-            using var request = new UnityWebRequest($"{url}/{roomId}",UnityWebRequest.kHttpVerbPOST)
-            {
-                uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(JsonMapper.ToJson(sendInfo)))
-            };
-            request.SetRequestHeader("Content-Type","application/json");
-            //Debug.Log($"SEND\nURL:{request.url}\nmessage:{JsonMapper.ToJson(sendInfo)}");
-            request.SendWebRequest().completed += x => { Debug.Log($"Send End {x}");};
-            */
         }
         public static async UniTask<ReceiveInfo> Get(string url,string roomId) {
             using var request = UnityWebRequest.Get($"{url}/{roomId}");
             await request.SendWebRequest();
-            //Debug.Log($"GET\nURL:{request.url}\nmessage:{request.downloadHandler.text}");
             var receiveInfo = JsonMapper.ToObject<ReceiveInfo>(request.downloadHandler.text);
             receiveInfo.Log();
-            //Debug.Log("END:GET");
             return receiveInfo;
         }
     }
